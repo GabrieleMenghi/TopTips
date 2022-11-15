feb7561c72189edfb7f98670eae5cb06bfa28cb2
Giovanni Muccioli <giovannimuccioli@outlook.com>
2022-11-15 15:20:40 +0100

    Add a simple user date insertion

---
+++ database/inserimento_dati.sql
@@ -0,0 +1,3 @@
+INSERT INTO `utente` (`idutente`, `username`, `password`, `nome`) VALUES
+('giovannimuccioli@gmail.com', 'admin1', 'Giovanni Muccioli'),
+('gabrielemenghi@gmail.com', 'admin2', 'Gabriele Menghi');
