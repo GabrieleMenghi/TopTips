ebed0196b8c8c7e9cde5d876c0cd439e6d3f8268
Giovanni Muccioli <giovannimuccioli@outlook.com>
2022-11-18 11:20:52 +0100

    Remove attribute from user insert

--- database/inserimento_dati.sql
+++ database/inserimento_dati.sql
@@ -1,3 +1,3 @@
-INSERT INTO `utente` (`idutente`, `username`, `password`, `nome`) VALUES
+INSERT INTO `utente` (`username`, `password`, `nome`) VALUES
 ('giovannimuccioli@gmail.com', 'admin1', 'Giovanni Muccioli'),
 ('gabrielemenghi@gmail.com', 'admin2', 'Gabriele Menghi');
