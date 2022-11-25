411846d8d85ca4f8fd02d0fd3ccc5565d6460232
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-11-25 11:23:53 +0100

    Add navigation menù (desktop version)

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+const sidebar = document.querySelector('.sidebar');
+const navItems = document.querySelectorAll('nav .nav-item');
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+
+    if(sidebar.className === 'sidebar')
+    {
+        sidebar.classList.add('open');
+        toggle.classList.add('open');
+    }
+    else
+    {
+        sidebar.classList.remove('open');
+        toggle.classList.remove('open');
+    }
+});
+
+navItems.forEach(navItem => {
+    navItem.addEventListener('click', () => {
+        navItems.forEach(navItem => {
+            navItem.classList.remove('active');
+        })
+        navItem.classList.add('active');
+    });
+});
