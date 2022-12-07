aa85c909ba339a14457a2f5f1c80ff6ea8170c58
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-12-07 20:38:40 +0100

    Link desktop and mobile icons

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,13 @@ const navItems = document.querySelectorAll('nav .nav-item');
 const toggle = document.querySelector('.toggle');
 const footerMobile = document.querySelectorAll('.footer-mobile .nav-item');
 
+const homeIcon = document.querySelectorAll('.i-home');
+const bellIcon = document.querySelectorAll('.i-bell');
+const postIcon = document.querySelectorAll('.i-post');
+const userIcon = document.querySelectorAll('.i-user');
+
+const bell = document.querySelector('.notification .i-bell');
+
 toggle.addEventListener('click', () => {
 
     if(sidebar.className === 'sidebar')
@@ -17,20 +24,75 @@ toggle.addEventListener('click', () => {
     }
 });
 
+
+//Cambio pagina in versione desktop
 navItems.forEach(navItem => {
     navItem.addEventListener('click', () => {
         navItems.forEach(navItem => {
             navItem.classList.remove('active');
         })
-        navItem.classList.add('active');
+        footerMobile.forEach(navItem => {
+            navItem.classList.remove('active');
+        })
+        bellIcon.forEach(icon => {
+            icon.classList.remove('active');
+        });
+        if(navItem.classList.contains('i-bell')){
+            bellIcon.forEach(icon => {
+                icon.classList.add('active');
+            });
+        } else if(navItem.classList.contains('i-post')){
+            postIcon.forEach(icon => {
+                icon.classList.add('active');
+            });
+        } else if(navItem.classList.contains('i-user')){
+            userIcon.forEach(icon => {
+                icon.classList.add('active');
+            });
+        } else if(navItem.classList.contains('i-home')){
+            homeIcon.forEach(icon => {
+                icon.classList.add('active');
+            });
+        }
     });
 });
 
+//Cambio pagina in versione mobile
 footerMobile.forEach(navItem => {
     navItem.addEventListener('click', function() {
         footerMobile.forEach(navItem => {
             navItem.classList.remove('active');
         })
-        navItem.classList.add('active');
+        navItems.forEach(navItem => {
+            navItem.classList.remove('active');
+        })
+        bellIcon.forEach(icon => {
+            icon.classList.remove('active');
+        });
+        if(navItem.classList.contains('i-post')){
+            postIcon.forEach(icon => {
+                icon.classList.add('active');
+            });
+        } else if(navItem.classList.contains('i-user')){
+            userIcon.forEach(icon => {
+                icon.classList.add('active');
+            });
+        } else if(navItem.classList.contains('i-home')){
+            homeIcon.forEach(icon => {
+                icon.classList.add('active');
+            });
+        }
+    });
+});
+
+bell.addEventListener('click', () => {
+    footerMobile.forEach(navItem => {
+        navItem.classList.remove('active');
+    })
+    navItems.forEach(navItem => {
+        navItem.classList.remove('active');
+    })
+    bellIcon.forEach(icon => {
+        icon.classList.add('active');
     });
 });
