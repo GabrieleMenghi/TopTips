4f2b76e16c13ae613bbc0876de240ad94dc5da81
Giovanni Muccioli <giovannimuccioli@outlook.com>
2023-03-29 14:08:42 +0200

    Add the link to the Bootstrap javascript file

--- template/base.php
+++ template/base.php
@@ -2,13 +2,15 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link rel="stylesheet" href="./css/style.css" />
+<!-- Stile personalizzato -->
+<link href="./css/style.css" rel="stylesheet" />
 <!-- BoxIcons v2.1.2 -->
 <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet">
+
 <title><?php echo $templateParams["titolo"]; ?></title>
 </head>
 <body>
@@ -217,5 +219,6 @@
         }
         loadDoc();
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
