95c46bf6b90c605a3cb16bd8972ef3d257608616
Giovanni Muccioli <giovannimuccioli@outlook.com>
2023-04-07 16:54:02 +0200

    Fix bug in account registration

--- registrazione.php
+++ registrazione.php
@@ -1,8 +1,13 @@
 <?php
 require_once("bootstrap.php");
 
-$templateParams["titolo"] = "TopTips - Registrazione";
-$templateParams["nome"] = "template/registrazione-form.php";
+if(!isUserLoggedIn()){
+    $templateParams["titolo"] = "TopTips - Registrazione";
+    $templateParams["nome"] = "template/registrazione-form.php";
+}
+else {
+    header("location: exit.php");
+}
 
 require("template/base.php");
 ?>
